e71c592d2e2ef46ef98fd2e87f481b54c24c0932
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2014-11-25 20:57:00 +0100

    Publiser datadumper til data.ub.uio.no

--- publish.sh
+++ publish.sh
@@ -34,4 +34,17 @@ git add HUMEregister.xml HUMEregister.ttl
 git commit -m "Humord oppdatert fra Bibsys"
 git push origin master
 
+#==========================================================
+# Publish compressed dumps
+#==========================================================
+
+DUMPS_DIR=/projects/data.ub.uio.no/dumps
+
+cp HUMEregister.ttl humord.ttl
+bzip2 -k humord.ttl
+zip humord.ttl.zip humord.ttl
+cp humord.ttl.zip $DUMPS_DIR/
+cp humord.ttl.bz2 $DUMPS_DIR/
+rm *.bz2 *.zip
+rm humord.ttl
 
