c3dfcbad6dc9b4f881af129a71dd086ac44445dd
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2014-12-07 23:24:12 +0100

    Postprocess with Skosify, add ConceptScheme

--- Makefile
+++ Makefile
@@ -2,8 +2,12 @@
 
 all: HUMEregister.ttl
 
-HUMEregister.ttl: HUMEregister.rdf.xml
-	rapper -i rdfxml -o turtle HUMEregister.rdf.xml >| HUMEregister.ttl
+HUMEregister.ttl: HUMEregister.tmp.ttl
+	rm skosify.log
+	../tools/skosify-sort/skosify-sort.py -c skosify.ini vocabulary.ttl HUMEregister.tmp.ttl -o HUMEregister.ttl
+
+HUMEregister.tmp.ttl: HUMEregister.rdf.xml
+	rapper -i rdfxml -o turtle HUMEregister.rdf.xml >| HUMEregister.tmp.ttl
 
 HUMEregister.rdf.xml: HUMEregister.xml
 	zorba -i convert.xq >| HUMEregister.rdf.xml
@@ -12,6 +16,8 @@ HUMEregister.xml:
 	wget -nv http://www.bibsys.no/files/out/humordsok/HUMEregister.xml
 
 clean:
+	rm skosify.log
 	rm HUMEregister.rdf.xml
 	rm HUMEregister.ttl
+	rm HUMEregister.tmp.ttl
 	rm HUMEregister.xml
--- publish.sh
+++ publish.sh
@@ -1,5 +1,58 @@
 #!/bin/sh
 
+function install_deps
+{
+    echo Installing/updating dependencies
+    pip install -U git+git://github.com/danmichaelo/skosify.git
+    xc=$?
+
+    if [ $xc != 0 ]; then
+        echo
+        echo -----------------------------------------------------------
+        echo ERROR:
+        echo Could not install dependencies using pip
+        echo -----------------------------------------------------------
+        exit 1
+    fi
+}
+
+cd ..
+
+if [ ! -f ENV/bin/activate ]; then
+
+    echo
+    echo -----------------------------------------------------------
+    echo Virtualenv not found. Trying to set up
+    echo -----------------------------------------------------------
+    echo
+    virtualenv ENV
+    xc=$?
+
+    if [ $xc != 0 ]; then
+        echo
+        echo -----------------------------------------------------------
+        echo ERROR:
+        echo Virtualenv exited with code $xc.
+        echo You may need to install or configure it.
+        echo -----------------------------------------------------------
+        exit 1
+    fi
+
+    echo Activating virtualenv
+    . ENV/bin/activate
+
+    install_deps
+
+else
+
+    echo Activating virtualenv
+    . ENV/bin/activate
+
+fi
+
+cd humord
+
+
 git checkout master
 git pull origin master
 
@@ -30,7 +83,7 @@ if [ $xc != 0 ]; then
 
 fi
 
-git add HUMEregister.xml HUMEregister.ttl
+git add HUMEregister.xml HUMEregister.ttl skosify.log
 git commit -m "Humord oppdatert fra Bibsys"
 git push origin master
 
