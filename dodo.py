8e0493d3a91fbf15e277f38482ebeaf6c2fded1e
Dan Michael O. Heggø <danmichaelo@gmail.com>
2020-11-20 13:42:28 +0100

    Configure reply-to

--- dodo.py
+++ dodo.py
@@ -126,6 +126,7 @@ def task_build_core():
             'domain': os.getenv('MAIL_DOMAIN'),
             'apikey': os.getenv('MAIL_APIKEY'),
             'sender': os.getenv('MAIL_SENDER'),
+            'reply': os.getenv('MAIL_REPLY'),
             'recipients': os.getenv('MAIL_RECIPIENTS').split(','),
         })
         roald.load('src/humord.marc21.xml', format='marc21', language='nb')
@@ -182,6 +183,7 @@ def task_build_extras():
             'domain': os.getenv('MAIL_DOMAIN'),
             'apikey': os.getenv('MAIL_APIKEY'),
             'sender': os.getenv('MAIL_SENDER'),
+            'reply': os.getenv('MAIL_REPLY'),
             'recipients': os.getenv('MAIL_RECIPIENTS').split(','),
         })
         roald.load('src/humord.marc21.xml', format='marc21', language='nb')
