e712724b5cd23c877bdfcc18bcd3aa1f780ca8e9
Dan Michael O. Heggø <danmichaelo@gmail.com>
2020-11-20 13:18:46 +0100

    Add Mailer

--- dodo.py
+++ dodo.py
@@ -122,7 +122,12 @@ def task_build_core():
 
     def build(task):
         logger.info('Building new core dist')
-        roald = Roald()
+        roald = Roald(mail_config={
+            'domain': os.getenv('MAIL_DOMAIN'),
+            'apikey': os.getenv('MAIL_APIKEY'),
+            'sender': os.getenv('MAIL_SENDER'),
+            'recipients': os.getenv('MAIL_RECIPIENTS').split(','),
+        })
         roald.load('src/humord.marc21.xml', format='marc21', language='nb')
         roald.set_uri_format(
             'http://data.ub.uio.no/%s/c{id}' % config['basename'])
